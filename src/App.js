d07ec219bbaf891f903e3ece0fc17b6300dd51c2
Richard <charichard09@gmail.com>
2023-06-27 19:54:44 -0700

    add css to header
    Co-authored-by: joshgarcia7410@gmail.com

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mens" element={<Home />} />
+        <Route path="/womens" element={<Home />} />
       </Routes>
       {/* <Footer /> */}
     </BrowserRouter>
--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,33 @@
 import { NavLink } from "react-router-dom";
+import logo from '../assets/logo.jpg';
 
 export default function Header() {
 
   return (
   <div>
-<header>
-  <NavLink to="/" style={({ isActive }) =>
-        isActive ? {textDecoration: "underline", "paddingRight": "1em", color: "black"} : { textDecoration: "none", "paddingRight": "1em", color: "black" } }>
-          Home
-  </NavLink>
-  <NavLink to="/about" style={({ isActive }) =>
-        isActive ? {textDecoration: "underline", "paddingRight": "1em", color: "black"} : { textDecoration: "none", "paddingRight": "1em", color: "black" }}>
-    About
-  </NavLink>
-</header>
-</div>
+    <header>
+      <div class="nav-bar">
+        <div class="logo-container">
+          <NavLink to="/">
+            <img src={logo}  alt="KewlLogo" />
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/about" style={({ isActive }) =>
+            isActive ? {textDecoration: "underline", "paddingRight": "1em", color: "black"} : { textDecoration: "none", "paddingRight": "1em", color: "black" }}>
+            About
+          </NavLink>
+          <NavLink to="/mens" style={({ isActive }) =>
+            isActive ? {textDecoration: "underline", "paddingRight": "1em", color: "black"} : { textDecoration: "none", "paddingRight": "1em", color: "black" }}>
+            Mens
+          </NavLink>
+          <NavLink to="/women" style={({ isActive }) =>
+            isActive ? {textDecoration: "underline", "paddingRight": "1em", color: "black"} : { textDecoration: "none", "paddingRight": "1em", color: "black" }}>
+            Womens
+          </NavLink>
+        </div>
+      </div>
+    </header>
+  </div>
   );
 }
