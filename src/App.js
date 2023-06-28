5010ce65296c41d4abd9ca8cb181b58fdd58c243
Richard <charichard09@gmail.com>
2023-06-27 19:17:56 -0700

    remove default create-react content
    Co-authored-by: joshgarcia7410@gmail.com

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home';
 import Header from './components/header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
 function App() {
-  let home = <Home />;
   return (
     <BrowserRouter>
       <Header />
